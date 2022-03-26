48d7ef8786d505b172335e570453184e0f1be1af
Karthickjuz <rkarthick14699@gmail.com>
2022-03-26 17:22:59 +0530

    Hello Git

---
+++ src/org/hub/Githubb.java
@@ -0,0 +1,14 @@
+package org.hub;
+
+public class Githubb {
+	private void add() {
+		System.out.println("Karthick");
+
+	}
+
+	public static void main(String[] args) {
+		Githubb v =new Githubb();
+	v.add();
+	}
+
+}
